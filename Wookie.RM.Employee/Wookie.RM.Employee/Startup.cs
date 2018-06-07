96885a9fe9494970bc044a12c49b4ab7ddb6caf9
Frederic Bluth <Frederic.Bluth@gmail.com>
2018-06-07 12:12:53 +0200

    Wookie.RM.Employee added

---
+++ Wookie.RM.Employee/Wookie.RM.Employee/Startup.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Wookie.RM.Employee
+{
+    public class Startup
+    {
+        public XtraUserControl Create(SqlConnection sqlConnectionMasterDB, SqlConnection sqlConnectionClientDB, long? FKExternal)
+        {
+            MasterData.SqlConnectionMasterDB = sqlConnectionMasterDB;
+            MasterData.SqlConnectionClientDB = sqlConnectionClientDB;
+            MasterData.FKContactData = FKExternal;
+
+            return new ucContact();
+        }
+    }
+
+    public static class MasterData
+    {
+        public static SqlConnection SqlConnectionClientDB;
+        public static SqlConnection SqlConnectionMasterDB;
+        public static long? FKContactData;
+    }
+}
